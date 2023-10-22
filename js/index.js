51efc1e18f814bd6c044eed86af7f207dbbceb00
Heloisa Felizardo <helocmp@yahoo.com.br>
2023-10-22 10:36:40 -0300

    feat: fetch - Renderiza a entrada quando o POST é bem-sucedido, Adiciona entrada à lista existente, Não codifica a resposta

--- js/index.js
+++ js/index.js
@@ -1,18 +1,25 @@
-import FetchWrapper from './module/fetch-wrapper.js';
+//Este código lida com o envio de dados de alimentos para uma API Firestore e exibe os alimentos na lista após a submissão do formulário.
 
-const API = new FetchWrapper('https://firestore.googleapis.com/v1/projects/jsdemo-3f387/databases/(default)/documents');
+// Importa a classe FetchWrapper do arquivo fetch-wrapper.js.
+import FetchWrapper from './fetch-wrapper.js';
 
+// Cria uma instância da classe FetchWrapper, que será usada para fazer requisições à API Firestore.
+const API = new FetchWrapper('https://firestore.googleapis.com/v1/projects/jsdemo-3f387/databases/(default)/documents/heloisa');
+
+// Obtém referências para elementos HTML usando seus IDs.
+const list = document.querySelector('#food-list');
 const form = document.querySelector('#create-form');
 const name = document.querySelector('#create-name');
 const carbs = document.querySelector('#create-carbs');
 const protein = document.querySelector('#create-protein');
 const fat = document.querySelector('#create-fat');
-const message = document.querySelector('.tooltiptext');
 
-form.addEventListener('submit', (e) => {
-	e.preventDefault();
+// Adiciona um ouvinte de evento para o evento de envio do formulário.
+form.addEventListener('submit', (event) => {
+	event.preventDefault(); // Impede o envio padrão do formulário.
 
-	API.post('/heloisa', {
+	// Faz uma requisição POST para a API Firestore com os valores dos campos do formulário.
+	API.post('/', {
 		fields: {
 			name: { stringValue: name.value },
 			carbs: { integerValue: carbs.value },
@@ -21,12 +28,30 @@ form.addEventListener('submit', (e) => {
 		},
 	}).then((data) => {
 		console.log(data);
+
+		// Verifica se há um erro na resposta da API.
 		if (data.error) {
-			// there was an error
-			message.classList.add('show');
+			// Havia um erro, não faz nada e retorna.
 			return;
 		}
-		message.classList.remove('show');
+
+		// Insere um novo item na lista de alimentos no HTML com os valores do formulário.
+		list.insertAdjacentHTML(
+			'beforeend',
+			`<li class="card">
+        <div>
+          <h3 class="name">${name.value}</h3>
+          <div class="calories">0 calories</div>
+          <ul class="macros">
+            <li class="carbs"><div>Carbs</div><div class="value">${carbs.value}g</div></li>
+            <li class="protein"><div>Protein</div><div class="value">${protein.value}g</div></li>
+            <li class="fat"><div>Fat</div><div class="value">${fat.value}g</div></li>
+          </ul>
+        </div>
+      </li>`,
+		);
+
+		// Limpa os campos do formulário após a submissão.
 		name.value = '';
 		carbs.value = '';
 		protein.value = '';
--- js/module/fetch-wrapper.js
+++ js/module/fetch-wrapper.js
@@ -1,26 +1,40 @@
+// Classe que fornece métodos de envio de requisições HTTP usando a API Fetch.
 export default class FetchWrapper {
+	// Construtor da classe que recebe a URL base da API como argumento.
 	constructor(baseURL) {
 		this.baseURL = baseURL;
 	}
 
+	// Método assíncrono que faz uma requisição GET para o endpoint especificado.
 	async get(endpoint) {
+		// Faz uma requisição GET para a URL composta pela baseURL e o endpoint.
 		const response = await fetch(this.baseURL + endpoint);
+		// Retorna os dados da resposta no formato JSON.
 		return await response.json();
 	}
 
+	// Método que faz uma requisição PUT para o endpoint especificado com o corpo especificado.
 	put(endpoint, body) {
+		// Chama o método #send com o método HTTP 'put', endpoint e corpo da requisição.
 		return this.#send('put', endpoint, body);
 	}
 
+	// Método que faz uma requisição POST para o endpoint especificado com o corpo especificado.
 	post(endpoint, body) {
+		// Chama o método #send com o método HTTP 'post', endpoint e corpo da requisição.
 		return this.#send('post', endpoint, body);
 	}
 
+	// Método que faz uma requisição DELETE para o endpoint especificado com o corpo especificado.
 	delete(endpoint, body) {
+		// Chama o método #send com o método HTTP 'delete', endpoint e corpo da requisição.
 		return this.#send('delete', endpoint, body);
 	}
 
+	// Método privado (indicado pelo '#' antes do nome) para enviar uma requisição HTTP genérica.
 	async #send(method, endpoint, body) {
+		// Faz uma requisição usando o método especificado, a URL composta pela baseURL e o endpoint,
+		// e um objeto de configuração com cabeçalhos e corpo em formato JSON.
 		const response = await fetch(this.baseURL + endpoint, {
 			method,
 			headers: {
@@ -28,6 +42,7 @@ export default class FetchWrapper {
 			},
 			body: JSON.stringify(body),
 		});
+		// Retorna os dados da resposta no formato JSON.
 		return await response.json();
 	}
 }
