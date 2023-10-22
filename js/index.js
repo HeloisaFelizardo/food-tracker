7250593a9beaeaa95dd666d8b88b4f3c034fc349
Heloisa Felizardo <helocmp@yahoo.com.br>
2023-10-22 11:34:55 -0300

    modified: index.css, mudança do inglês para português

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
 //Este código lida com o envio de dados de alimentos para uma API Firestore e exibe os alimentos na lista após a submissão do formulário.
 
 // Importa a classe FetchWrapper do arquivo fetch-wrapper.js.
-import FetchWrapper from './module/fetch-wrapper.js';
+import FetchWrapper from './class/fetch-wrapper.js';
+import { capitalize, calculateCalories } from './helpers.js';
 
 // Cria uma instância da classe FetchWrapper, que será usada para fazer requisições à API Firestore.
 const API = new FetchWrapper('https://firestore.googleapis.com/v1/projects/jsdemo-3f387/databases/(default)/documents/heloisa');
@@ -40,12 +41,12 @@ form.addEventListener('submit', (event) => {
 			'beforeend',
 			`<li class="card">
         <div>
-          <h3 class="name">${name.value}</h3>
-          <div class="calories">0 calories</div>
+          <h3 class="name">${capitalize(name.value)}</h3>
+          <div class="calories">${calculateCalories(carbs.value, protein.value, fat.value)} calories</div>
           <ul class="macros">
-            <li class="carbs"><div>Carbs</div><div class="value">${carbs.value}g</div></li>
-            <li class="protein"><div>Protein</div><div class="value">${protein.value}g</div></li>
-            <li class="fat"><div>Fat</div><div class="value">${fat.value}g</div></li>
+            <li class="carbs"><div>Calorias</div><div class="value">${carbs.value}g</div></li>
+            <li class="protein"><div>Proteína</div><div class="value">${protein.value}g</div></li>
+            <li class="fat"><div>Gordura</div><div class="value">${fat.value}g</div></li>
           </ul>
         </div>
       </li>`,
